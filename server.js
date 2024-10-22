c9edade52e285f35dca709b31b12671e6a8cadf6
soumyanil22 <soumyanil22@gmail.com>
2024-10-22 11:32:28 +0530

    fix: cookie conf

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
+// Initialize 1
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Configure session middleware
 app.use(
   session({
@@ -26,18 +30,14 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      path: '/',
+      path: "/",
       maxAge: 1000 * 60 * 30, // 30 minutes in milliseconds
       sameSite: "none",
       secure: true,
-      domain: 'aicloserx.com',
+      domain: "aicloserx.com",
     },
   })
 );
-
-// Initialize 1
-app.use(passport.initialize());
-app.use(passport.session());
 
 // Configure Passport with Google OAuth strategy
 passport.use(
